e35f16ad8db2b965c29d1ebb8bb4b5469bca18d5
James <jchalmers811@gmail.com>
2018-08-01 11:34:49 +1200

    Firebase email registration and login works

--- app/src/main/java/com/example/james/flatchat/MainActivity.kt
+++ app/src/main/java/com/example/james/flatchat/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.james.flatchat
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,15 +14,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        // access to text
-        val username = username_edittext_login.text.toString()
-        val password = password_edittext_login.text.toString()
-
         // listen for button press
         login_button_login.setOnClickListener {
 
-            // TODO: registration logic
+            performLogin()
+
         }
 
         makeaccount_textview_login.setOnClickListener {
@@ -32,4 +31,33 @@ class MainActivity : AppCompatActivity() {
 
 
     }
+
+    private fun performLogin() {
+
+        // access to text
+        val email = email_edittext_login.text.toString()
+        val password = password_edittext_login.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter a valid email and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+                    //else
+                    Log.d("Main", "Login Successful with UID: ${it.result.user.uid}")
+                }
+                .addOnFailureListener {
+                    Log.d("Main", "Failed to login: ${it.message}")
+                    Toast.makeText(this, "Failed to login: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
+
+
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/james/flatchat/RegisterActivity.kt
+++ app/src/main/java/com/example/james/flatchat/RegisterActivity.kt
@@ -2,6 +2,10 @@ package com.example.james.flatchat
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
@@ -11,17 +15,36 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(R.layout.activity_register)
 
         register_button_register.setOnClickListener {
-
-
+            performRegister()
         }
 
         backtologin_textview_register.setOnClickListener {
             finish()
-
         }
 
 
 
+    }
+
+    private fun performRegister(){
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(this, "Please enter a valid email and password", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if (!it.isSuccessful) return@addOnCompleteListener
+                    Log.d("Main", "Registration succesful with UID: ${it.result.user.uid}")
+                }
+
+                .addOnFailureListener {
+                    Log.d("Main", "Failed to create user: ${it.message}")
+                    Toast.makeText(this, "Failed to create user: ${it.message}", Toast.LENGTH_SHORT).show()
+                }
     }
 
 
